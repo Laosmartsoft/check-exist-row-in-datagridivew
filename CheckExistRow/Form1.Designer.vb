1b3488c54e70fcb01909b4475419213936e93763
Laosmartsoft <hackerdone@gmail.com>
2020-09-06 00:27:37 +0700

    first commit

---
+++ CheckExistRow/Form1.Designer.vb
@@ -0,0 +1,215 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.txtBarcode = New System.Windows.Forms.TextBox()
+        Me.txtProductName = New System.Windows.Forms.TextBox()
+        Me.txtPrice = New System.Windows.Forms.TextBox()
+        Me.txtAmount = New System.Windows.Forms.TextBox()
+        Me.txtTotal = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.dgvSale = New System.Windows.Forms.DataGridView()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.barcode = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.product_name = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.price = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.amount = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.total = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        CType(Me.dgvSale, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'txtBarcode
+        '
+        Me.txtBarcode.Location = New System.Drawing.Point(33, 92)
+        Me.txtBarcode.Name = "txtBarcode"
+        Me.txtBarcode.Size = New System.Drawing.Size(128, 20)
+        Me.txtBarcode.TabIndex = 0
+        '
+        'txtProductName
+        '
+        Me.txtProductName.Location = New System.Drawing.Point(167, 92)
+        Me.txtProductName.Name = "txtProductName"
+        Me.txtProductName.Size = New System.Drawing.Size(217, 20)
+        Me.txtProductName.TabIndex = 1
+        '
+        'txtPrice
+        '
+        Me.txtPrice.Location = New System.Drawing.Point(390, 92)
+        Me.txtPrice.Name = "txtPrice"
+        Me.txtPrice.Size = New System.Drawing.Size(128, 20)
+        Me.txtPrice.TabIndex = 2
+        '
+        'txtAmount
+        '
+        Me.txtAmount.Location = New System.Drawing.Point(524, 92)
+        Me.txtAmount.Name = "txtAmount"
+        Me.txtAmount.Size = New System.Drawing.Size(64, 20)
+        Me.txtAmount.TabIndex = 3
+        '
+        'txtTotal
+        '
+        Me.txtTotal.Location = New System.Drawing.Point(594, 92)
+        Me.txtTotal.Name = "txtTotal"
+        Me.txtTotal.Size = New System.Drawing.Size(128, 20)
+        Me.txtTotal.TabIndex = 4
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(30, 76)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(47, 13)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "Barcode"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(164, 76)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(75, 13)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "Product Name"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(387, 76)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(31, 13)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "Price"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(521, 76)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(43, 13)
+        Me.Label4.TabIndex = 8
+        Me.Label4.Text = "Amount"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(591, 76)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(31, 13)
+        Me.Label5.TabIndex = 9
+        Me.Label5.Text = "Total"
+        '
+        'dgvSale
+        '
+        Me.dgvSale.AllowUserToAddRows = False
+        Me.dgvSale.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvSale.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.barcode, Me.product_name, Me.price, Me.amount, Me.total})
+        Me.dgvSale.Location = New System.Drawing.Point(33, 118)
+        Me.dgvSale.Name = "dgvSale"
+        Me.dgvSale.RowHeadersVisible = False
+        Me.dgvSale.Size = New System.Drawing.Size(689, 249)
+        Me.dgvSale.TabIndex = 10
+        '
+        'btnAdd
+        '
+        Me.btnAdd.Location = New System.Drawing.Point(33, 26)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(75, 23)
+        Me.btnAdd.TabIndex = 11
+        Me.btnAdd.Text = "Add"
+        Me.btnAdd.UseVisualStyleBackColor = True
+        '
+        'barcode
+        '
+        Me.barcode.HeaderText = "Barcode"
+        Me.barcode.Name = "barcode"
+        Me.barcode.Width = 120
+        '
+        'product_name
+        '
+        Me.product_name.HeaderText = "ProductName"
+        Me.product_name.Name = "product_name"
+        Me.product_name.Width = 220
+        '
+        'price
+        '
+        Me.price.HeaderText = "Price"
+        Me.price.Name = "price"
+        '
+        'amount
+        '
+        Me.amount.HeaderText = "Amount"
+        Me.amount.Name = "amount"
+        '
+        'total
+        '
+        Me.total.HeaderText = "Total"
+        Me.total.Name = "total"
+        Me.total.Width = 130
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(749, 394)
+        Me.Controls.Add(Me.btnAdd)
+        Me.Controls.Add(Me.dgvSale)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtTotal)
+        Me.Controls.Add(Me.txtAmount)
+        Me.Controls.Add(Me.txtPrice)
+        Me.Controls.Add(Me.txtProductName)
+        Me.Controls.Add(Me.txtBarcode)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.dgvSale, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtBarcode As TextBox
+    Friend WithEvents txtProductName As TextBox
+    Friend WithEvents txtPrice As TextBox
+    Friend WithEvents txtAmount As TextBox
+    Friend WithEvents txtTotal As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents dgvSale As DataGridView
+    Friend WithEvents btnAdd As Button
+    Friend WithEvents barcode As DataGridViewTextBoxColumn
+    Friend WithEvents product_name As DataGridViewTextBoxColumn
+    Friend WithEvents price As DataGridViewTextBoxColumn
+    Friend WithEvents amount As DataGridViewTextBoxColumn
+    Friend WithEvents total As DataGridViewTextBoxColumn
+End Class
---
+++ CheckExistRow/Form1.vb
@@ -0,0 +1,40 @@
+﻿Public Class Form1
+    Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+
+
+        If (dgvSale.Rows.Count > 0) Then
+
+            For i = 0 To dgvSale.Rows.Count - 1
+
+                If (dgvSale.Rows(i).Cells("barcode").Value = txtBarcode.Text) Then
+
+                    dgvSale.Rows(i).Cells("amount").Value += CInt(txtAmount.Text)
+                    dgvSale.Rows(i).Cells("total").Value = (CInt(dgvSale.Rows(i).Cells("amount").Value) * CInt(dgvSale.Rows(i).Cells("price").Value)).ToString("#,##0")
+
+                    Exit Sub
+                End If
+
+
+            Next
+
+        End If
+
+
+        dgvSale.Rows.Add(txtBarcode.Text, txtProductName.Text, txtPrice.Text, txtAmount.Text, txtTotal.Text)
+
+    End Sub
+
+    Private Sub txtAmount_TextChanged(sender As Object, e As EventArgs) Handles txtAmount.TextChanged
+        Try
+            If txtAmount.Text <> "" And txtPrice.Text <> "" Then
+
+                Dim total As Integer = CInt(txtAmount.Text) * CInt(txtPrice.Text)
+                txtTotal.Text = total.ToString("#,##0")
+
+            End If
+
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+End Class
